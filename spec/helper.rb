1612fc40d38a180fa687b7eff99eeab9500911cb
John Nunemaker <nunemaker@gmail.com>
2013-02-06 11:32:19 -0500

    Clean up spec helper.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,21 +1,10 @@
 $:.unshift(File.expand_path('../../lib', __FILE__))
 
-require 'pathname'
-require 'logger'
-
-root_path = Pathname(__FILE__).dirname.join('..').expand_path
-lib_path  = root_path.join('lib')
-log_path  = root_path.join('log')
-log_path.mkpath
-
 require 'rubygems'
 require 'bundler'
-
-Bundler.require(:default, :test)
+Bundler.setup :default
 
 require 'flipper-ui'
-
-Logger.new(log_path.join('test.log'))
 
 RSpec.configure do |config|
   config.filter_run :focused => true
