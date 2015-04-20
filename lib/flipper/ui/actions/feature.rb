51b5b996181b1d7c7900fb38cd9b7966a03fb533
John Nunemaker <nunemaker@gmail.com>
2015-04-20 13:07:40 -0400

    Share percentages with ivar

--- lib/flipper/ui/actions/feature.rb
+++ lib/flipper/ui/actions/feature.rb
@@ -12,6 +12,7 @@ module Flipper
           feature_name = Rack::Utils.unescape(request.path.split("/").last)
           @feature = Decorators::Feature.new(flipper[feature_name])
           @page_title = "#{@feature.key} // Features"
+          @percentages = [0, 1, 5, 10, 15, 25, 50, 75, 100]
 
           view_response :feature
         end
