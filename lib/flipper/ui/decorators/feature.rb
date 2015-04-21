c10b16e41e1944aece6647f2ba85e47eb32f248e
John Nunemaker <nunemaker@gmail.com>
2015-04-21 12:00:10 -0400

    Make features sortable

--- lib/flipper/ui/decorators/feature.rb
+++ lib/flipper/ui/decorators/feature.rb
@@ -5,6 +5,8 @@ module Flipper
   module UI
     module Decorators
       class Feature < SimpleDelegator
+        include Comparable
+
         # Public: The feature being decorated.
         alias_method :feature, :__getobj__
 
@@ -25,6 +27,20 @@ module Flipper
               Decorators::Gate.new(gate, gate_values[gate.key]).as_json
             },
           }
+        end
+
+        StateSortMap = {
+          :on => 1,
+          :conditional => 2,
+          :off => 3,
+        }
+
+        def <=>(other)
+          if state == other.state
+            key <=> other.key
+          else
+            StateSortMap[state] <=> StateSortMap[other.state]
+          end
         end
       end
     end
